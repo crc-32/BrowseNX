aeb41ead143a8861ab980045111ed29188160909
crc-32 <thomas.c.england@gmail.com>
2019-01-21 16:39:05 +0000

    Add linux build binary

--- build.sh
+++ build.sh
@@ -16,10 +16,10 @@ make
 cp ./Browser.nso ./ExeFs/main
 npdmtool ./npdm.json ./ExeFs/main.npdm
 cp ./Browser.nacp ./Control/control.nacp
-./Build-Tools/hacpack -k ./prod.keys --type nca --ncatype manual --titleid 0100C4C320C0FFEE --romfsdir=OfflineHtml --outdir=Build.html
-./Build-Tools/hacpack -k ./prod.keys --type nca --ncatype program --titleid 0100C4C320C0FFEE --exefsdir=ExeFs --romfsdir=RomFs --logodir=Logo --outdir=Build.program
-./Build-Tools/hacpack -k ./prod.keys --type nca --ncatype control --titleid 0100C4C320C0FFEE --romfsdir=Control --outdir=Build.control
-./Build-Tools/hacpack -k ./prod.keys --type nca --ncatype meta --titleid 0100C4C320C0FFEE --titletype application --programnca Build.program/*.nca --controlnca Build.control/*.nca --htmldocnca Build.html/*.nca --outdir=Build.meta
+./hacpack -k ./prod.keys --type nca --ncatype manual --titleid 0100C4C320C0FFEE --romfsdir=OfflineHtml --outdir=Build.html
+./hacpack -k ./prod.keys --type nca --ncatype program --titleid 0100C4C320C0FFEE --exefsdir=ExeFs --romfsdir=RomFs --logodir=Logo --outdir=Build.program
+./hacpack -k ./prod.keys --type nca --ncatype control --titleid 0100C4C320C0FFEE --romfsdir=Control --outdir=Build.control
+./hacpack -k ./prod.keys --type nca --ncatype meta --titleid 0100C4C320C0FFEE --titletype application --programnca Build.program/*.nca --controlnca Build.control/*.nca --htmldocnca Build.html/*.nca --outdir=Build.meta
 cp ./Build*/*.nca ./Build.nsp/
-./Build-Tools/hacpack -k ./prod.keys --type nsp --titleid 0100C4C320C0FFEE --ncadir=Build.nsp --outdir=Output
+./hacpack -k ./prod.keys --type nsp --titleid 0100C4C320C0FFEE --ncadir=Build.nsp --outdir=Output
 mv Output/0100c4c320c0ffee.nsp Browser.nsp
